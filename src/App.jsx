8565140fbeee86612058b10d00737ffc13b4f767
unknown <abdulmumuni406@gmail.com>
2026-07-09 17:05:53 +0000

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Contact from './pages/contact'
 const App = () => {
   return (
     <>
       <Navbar/>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} /> 
+      <Route path="/contact" element={<Contact />} /> 
     </Routes>
     </>
   )
---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Map from '../components/Map'
+import Contacthero from '../components/Contacthero'
+const contact = () => {
+  return (
+    <div><Contacthero /><Map /></div>
+  )
+}
+
+export default contact
