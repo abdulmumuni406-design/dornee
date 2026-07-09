e8145a939d0e5c5d0370425df5aa2b4949b5efae
unknown <abdulmumuni406@gmail.com>
2026-07-09 17:09:02 +0000

    added

--- src/components/Contacthero.jsx
+++ src/components/Contacthero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contacthero = () => {
   return (
-    <div className='w-full h-[90vh] contact-hero-bg'>
+    <div className='w-full h-80 lg:h-[90vh] contact-hero-bg'>
         <div className="max-w-3xl mx-auto h-full flex flex-col justify-center items-center text-center px-4">
         <h2 className="text-xl lg:text-2xl">Your dream outfit begins with a conversation. Reach out to us today to schedule a consultation or discuss your custom design, and let us craft a garment that reflects your unique style with elegance and precision.</h2>
 </div>
